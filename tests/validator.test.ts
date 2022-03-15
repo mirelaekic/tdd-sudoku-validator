94e879d766d6d3568027c076de5d162d7fe83be7
Phillip Hamnett <phillip.hamnett@gmail.com>
2022-03-15 12:05:36 +0100

    Add test for sudoku with dodgy squares

--- tests/validator.test.ts
+++ tests/validator.test.ts
@@ -40,7 +40,6 @@ describe("Validate sudoku", (() => {
         expect(Validator.validateSudoku(invalidSudokuBadRow)).to.equal(false)
     }))
 
-
     it("should detect invalid sudoku with dodgy column", (() => {
         const invalidSudokuBadColumn = [
             [5, 3, 4, 6, 7, 8, 9, 1, 2],
@@ -54,5 +53,20 @@ describe("Validate sudoku", (() => {
             [3, 4, 5, 2, 8, 6, 1, 7, 9]
         ];
         expect(Validator.validateSudoku(invalidSudokuBadColumn)).to.equal(false)
+    }))
+
+    it("should detect invalid sudoku with dodgy squares", (() => {
+        const invalidSudokuBadSquare = [
+            [5, 3, 4, 6, 7, 8, 9, 1, 2],
+            [6, 5, 2, 1, 9, 7, 3, 4, 8],
+            [1, 9, 8, 3, 4, 2, 5, 6, 7],
+            [8, 7, 9, 3, 6, 1, 4, 2, 3],
+            [4, 2, 6, 8, 5, 3, 7, 9, 1],
+            [7, 1, 3, 9, 2, 4, 8, 5, 6],
+            [9, 6, 1, 5, 3, 7, 2, 8, 4],
+            [2, 8, 7, 4, 1, 9, 6, 3, 5],
+            [3, 4, 5, 2, 8, 6, 1, 7, 9]
+        ];
+        expect(Validator.validateSudoku(invalidSudokuBadSquare)).to.equal(false)
     }))
 }))
