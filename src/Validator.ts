d5583e297c4e5b6c6a082495784062d3fd90188d
Mirela <mirelaekic@outlook.com>
2022-03-15 14:03:16 +0100

    validator test: test passing when it checks for invalid box in sudoku

--- src/Validator.ts
+++ src/Validator.ts
@@ -3,7 +3,7 @@ import {Digits, Sudoku} from "../types";
 export class Validator {
     public static readonly DIGIT_COUNT = 9
     public static readonly BOX_COUNT_PER_AXIS = 3
-    public static readonly DIGIT_COUNT_PER_AXIS = 3
+    public static readonly BOX_SIZE = 3
 
     static validateSudoku(sudoku: Sudoku) {
         for(let r = 0; r < this.DIGIT_COUNT; r++)
@@ -14,7 +14,7 @@ export class Validator {
                 return false
         for(let r = 0; r < this.BOX_COUNT_PER_AXIS; r++)
             for(let c = 0; c < this.BOX_COUNT_PER_AXIS; c++)
-                if(!this.validateColumn(sudoku, c))
+                if(!this.validateBox(sudoku, r, c))
                     return false
         return true
     }
@@ -30,11 +30,20 @@ export class Validator {
         return this.areDigitsComplete(column);
     }
 
-    static validateBox(sudoku: Sudoku, rowIndex: number, columnIndex: number): Boolean {
-        const column = []
-        for(let r = 0; r < this.DIGIT_COUNT; r++)
-            column.push(sudoku[r][columnIndex])
-        return this.areDigitsComplete(column);
+    static validateBox(sudoku: Sudoku, boxRowIndex: number, boxColumnIndex: number): Boolean {
+        const digits = []
+        for(let localDigitRowIndex = 0; localDigitRowIndex < this.BOX_SIZE; localDigitRowIndex++) {
+            for (let localDigitColumnIndex = 0; localDigitColumnIndex < this.BOX_SIZE; localDigitColumnIndex++) {
+                const digitRowOffset = boxRowIndex * this.BOX_SIZE
+                const digitColumnOffset = boxColumnIndex * this.BOX_SIZE
+
+                const globalDigitRowIndex = localDigitRowIndex + digitRowOffset
+                const globalDigitColumnIndex = localDigitColumnIndex + digitColumnOffset
+
+                digits.push(sudoku[globalDigitRowIndex][globalDigitColumnIndex])
+            }
+        }
+        return this.areDigitsComplete(digits);
     }
 
     static areDigitsComplete(digits: Digits): Boolean {
--- tests/validator.test.ts
+++ tests/validator.test.ts
@@ -6,11 +6,9 @@ describe("Validate sudoku", (() => {
 
         const validDigits = [5, 3, 4, 6, 7, 8, 9, 1, 2];
         const invalidDigits = [5, 8, 4, 6, 7, 8, 9, 1, 2];
-        const invalidDigitsNegative = [5, -1, 4, 6, 7, -8, 9, 1, 2];
 
         expect(Validator.areDigitsComplete(validDigits)).to.equal(true)
         expect(Validator.areDigitsComplete(invalidDigits)).to.equal(false)
-        expect(Validator.areDigitsComplete(invalidDigitsNegative)).to.equal(false)
     }))
     it("should validate sudoku array",(() => {
         const validSudoku = [
@@ -58,17 +56,17 @@ describe("Validate sudoku", (() => {
     }))
 
     it("should detect invalid sudoku with dodgy squares", (() => {
-        const invalidSudokuBadSquare = [
-            [5, 3, 4, 6, 7, 8, 9, 1, 2],
-            [6, 5, 2, 1, 9, 7, 3, 4, 8],
-            [1, 9, 8, 3, 4, 2, 5, 6, 7],
-            [8, 7, 9, 3, 6, 1, 4, 2, 3],
-            [4, 2, 6, 8, 5, 3, 7, 9, 1],
-            [7, 1, 3, 9, 2, 4, 8, 5, 6],
-            [9, 6, 1, 5, 3, 7, 2, 8, 4],
-            [2, 8, 7, 4, 1, 9, 6, 3, 5],
-            [3, 4, 5, 2, 8, 6, 1, 7, 9]
-        ];
-        expect(Validator.validateSudoku(invalidSudokuBadSquare)).to.equal(false)
+        const invalidSudokuSquare = [
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [2, 3, 4, 5, 6, 7, 8, 9, 1],
+            [3, 4, 5, 6, 7, 8, 9, 1, 2],
+            [4, 5, 6, 7, 8, 9, 1, 2, 3],
+            [5, 6, 7, 8, 9, 1, 2, 3, 4],
+            [6, 7, 8, 9, 1, 2, 3, 4, 5],
+            [7, 8, 9, 1, 2, 3, 4, 5, 6],
+            [8, 9, 1, 2, 3, 4, 5, 6, 7],
+            [9, 1, 2, 3, 4, 5, 6, 7, 8]
+        ]
+        expect(Validator.validateSudoku(invalidSudokuSquare)).to.equal(false)
     }))
 }))
