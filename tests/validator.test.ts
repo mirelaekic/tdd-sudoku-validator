612912cb836b6f5fc6db5dd526aa6da848c0f9f8
Mirela <mirelaekic@outlook.com>
2022-03-15 11:06:48 +0100

    validator test: checking areDigitsComplete signature

--- tests/validator.test.ts
+++ tests/validator.test.ts
@@ -1,7 +1,9 @@
 import { expect } from 'chai';
+import {Validator} from "../src/Validator";
 
 describe("Validate sudoku", (() => {
     it("should contain 1-9 digits in array", (() => {
-        const mockArray = [5, 3, 4, 6, 7, 8, 9, 1, 2]
+        const mockArray = [5, 3, 4, 6, 7, 8, 9, 1, 2];
+        expect(Validator.areDigitsComplete(mockArray)).to.equal(true)
     }))
 }))
