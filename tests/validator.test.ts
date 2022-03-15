14166ce027db321bcfd85b748495d925e984f3da
Mirela <mirelaekic@outlook.com>
2022-03-15 11:15:21 +0100

    validator test: adds logic to validate array in the areDigitsComplete

--- tests/validator.test.ts
+++ tests/validator.test.ts
@@ -3,7 +3,12 @@ import {Validator} from "../src/Validator";
 
 describe("Validate sudoku", (() => {
     it("should contain 1-9 digits in array", (() => {
-        const mockArray = [5, 3, 4, 6, 7, 8, 9, 1, 2];
-        expect(Validator.areDigitsComplete(mockArray)).to.equal(true)
+
+        const validDigits = [5, 3, 4, 6, 7, 8, 9, 1, 2];
+        const invalidDigits = [5, 8, 4, 6, 7, 8, 9, 1, 2];
+
+        expect(Validator.areDigitsComplete(validDigits)).to.equal(true)
+        expect(Validator.areDigitsComplete(invalidDigits)).to.equal(false)
     }))
+
 }))
