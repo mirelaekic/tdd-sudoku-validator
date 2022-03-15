1f9f92d7b295f098cf6a0aead07370f56d4b1e33
Mirela <mirelaekic@outlook.com>
2022-03-15 11:33:06 +0100

    validator test: Made 'areDigitsComplete' function return false for invalid input arrays

--- src/Validator.ts
+++ src/Validator.ts
@@ -1,9 +1,16 @@
+import {Digits} from "../types";
+
 export class Validator {
+    public static readonly DIGIT_COUNT = 9
+
     static validateSudoku() {
 
     }
 
-    static areDigitsComplete(): Boolean {
-
+    static areDigitsComplete(digits: Digits): Boolean {
+        for(let d = 1; d <= this.DIGIT_COUNT; d++)
+            if(!digits.includes(d))
+                return false
+        return true
     }
 }
